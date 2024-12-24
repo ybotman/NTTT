ab5c3973f781014a475a747511d252617a22ba70
YbotMan <toby.balsley+YbotMan@gmail.com>
2024-12-24 12:00:13 -0500

    image optz and GitBuild Branch

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,9 @@ const nextConfig = {
   basePath: "/NTTT", // <-- Add this
   output: "export", // IMPORTANT for a static export build
   trailingSlash: true, // often helpful for GH Pages
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
