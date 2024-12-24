9a5b0cd337c1413ae0629e8b049854da6b305ec3
YbotMan <toby.balsley+YbotMan@gmail.com>
2024-12-24 01:19:50 -0500

    and a stupid package error

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  basePath: '/NTTT',   // <-- Add this
-  output: 'export',  // IMPORTANT for a static export build
-  trailingSlash: true // often helpful for GH Pages
+  basePath: "/NTTT", // <-- Add this
+  output: "export", // IMPORTANT for a static export build
+  trailingSlash: true, // often helpful for GH Pages
 };
 
 export default nextConfig;
